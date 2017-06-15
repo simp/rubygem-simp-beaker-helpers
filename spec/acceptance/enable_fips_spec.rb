9f9e7d23e74a56779077290a4dad990a8b712aee
nick-markowski <nmarkowski@keywcorp.com>
2017-06-15 14:30:40 +0000

    Updated FIPS test to check ENV[BEAKER_fips]

--- spec/acceptance/enable_fips_spec.rb
+++ spec/acceptance/enable_fips_spec.rb
@@ -1,11 +1,15 @@
-require 'spec_helper_fips'
+require 'spec_helper_acceptance'
 
 hosts.each do |host|
-  describe 'ensure FIPS in enabled' do
+  describe 'ensure FIPS mode matches ENV[BEAKER_fips]' do
     context "on #{host}" do
-     it 'check /proc/sys/crypto/fips_enabled = 1' do
+     it 'check /proc/sys/crypto/fips_enabled' do
         stdout = on(host, 'cat /proc/sys/crypto/fips_enabled').stdout.strip
-        expect(stdout).to eq("1")
+        if ENV['BEAKER_fips'] == 'yes'
+          expect(stdout).to eq("1")
+        else
+          expect(stdout).to eq("0")
+        end
       end 
     end
   end
