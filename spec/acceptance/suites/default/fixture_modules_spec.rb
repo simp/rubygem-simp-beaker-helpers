32cc1203fd2efc309f941bbaa662d5d7fd544980
Mike Riddle <michael.riddle@onyxpoint.com>
2025-06-23 11:24:46 -0400

    attempting to set the test to pending again

--- spec/acceptance/suites/default/fixture_modules_spec.rb
+++ spec/acceptance/suites/default/fixture_modules_spec.rb
@@ -8,7 +8,7 @@ context 'after copy_fixture_modules_to( hosts )' do
 
   describe "fact_on(default,'root_home')" do
     it 'does not return value of `root_home`' do
-      pending 'Conflicts with beaker_puppet_helpers' unless pfact_on(default, 'puppetversion') =~ %r{ ^7\. }
+      pending 'Conflicts with beaker_puppet_helpers' unless Gem::Version.new(facts[:puppetversion]) < Gem::Version.new('8')
       expect(Beaker::DSL::Helpers::FacterHelpers.fact_on(default, 'root_home').to_s).to eq ''
     end
   end
