26d5e4548266fd65fbbf2c009828c69c7a8a83e9
Mike Riddle <michael.riddle@onyxpoint.com>
2025-06-23 09:40:55 -0400

    Marking a bad test as pending until we can come up with a better solution to test pfact_on

--- spec/acceptance/suites/default/fixture_modules_spec.rb
+++ spec/acceptance/suites/default/fixture_modules_spec.rb
@@ -8,6 +8,7 @@ context 'after copy_fixture_modules_to( hosts )' do
 
   describe "fact_on(default,'root_home')" do
     it 'does not return value of `root_home`' do
+      pending "Conflicts with beaker_puppet_helpers"
       expect(Beaker::DSL::Helpers::FacterHelpers.fact_on(default, 'root_home').to_s).to eq ''
     end
   end
