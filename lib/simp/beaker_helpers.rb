e7f28f07f1c297a1ac737f7febfc57de5beb8693
Mike Riddle <mike@sicura.us>
2025-10-08 11:09:25 -0400

    (#261) Removed outdated (and incorrect) errata for Windows (#262)
    * (#261) Removed outdated (and incorrect) errata for Windows
    
    Fixes #261
    
    * Fixed logging message
    
    * Updated version and changelog entries
    
    * Corrected version in the CHANGELOG
    
    * Corrected new version entry

--- lib/simp/beaker_helpers.rb
+++ lib/simp/beaker_helpers.rb
@@ -937,38 +937,8 @@ module Simp::BeakerHelpers
     linux_errata(linux_suts) unless linux_suts.empty?
 
     unless windows_suts.empty?
-      block_on(windows_suts, run_in_parallel: @run_in_parallel) do |sut|
-        # Load the Windows requirements
-        require 'simp/beaker_helpers/windows'
-
-        # Install the necessary windows certificate for testing
-        #
-        # https://petersouter.xyz/testing-windows-with-beaker-without-cygwin/
-        geotrust_global_ca = <<~EOM.freeze
-        -----BEGIN CERTIFICATE-----
-        MIIDVDCCAjygAwIBAgIDAjRWMA0GCSqGSIb3DQEBBQUAMEIxCzAJBgNVBAYTAlVT
-        MRYwFAYDVQQKEw1HZW9UcnVzdCBJbmMuMRswGQYDVQQDExJHZW9UcnVzdCBHbG9i
-        YWwgQ0EwHhcNMDIwNTIxMDQwMDAwWhcNMjIwNTIxMDQwMDAwWjBCMQswCQYDVQQG
-        EwJVUzEWMBQGA1UEChMNR2VvVHJ1c3QgSW5jLjEbMBkGA1UEAxMSR2VvVHJ1c3Qg
-        R2xvYmFsIENBMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA2swYYzD9
-        9BcjGlZ+W988bDjkcbd4kdS8odhM+KhDtgPpTSEHCIjaWC9mOSm9BXiLnTjoBbdq
-        fnGk5sRgprDvgOSJKA+eJdbtg/OtppHHmMlCGDUUna2YRpIuT8rxh0PBFpVXLVDv
-        iS2Aelet8u5fa9IAjbkU+BQVNdnARqN7csiRv8lVK83Qlz6cJmTM386DGXHKTubU
-        1XupGc1V3sjs0l44U+VcT4wt/lAjNvxm5suOpDkZALeVAjmRCw7+OC7RHQWa9k0+
-        bw8HHa8sHo9gOeL6NlMTOdReJivbPagUvTLrGAMoUgRx5aszPeE4uwc2hGKceeoW
-        MPRfwCvocWvk+QIDAQABo1MwUTAPBgNVHRMBAf8EBTADAQH/MB0GA1UdDgQWBBTA
-        ephojYn7qwVkDBF9qn1luMrMTjAfBgNVHSMEGDAWgBTAephojYn7qwVkDBF9qn1l
-        uMrMTjANBgkqhkiG9w0BAQUFAAOCAQEANeMpauUvXVSOKVCUn5kaFOSPeCpilKIn
-        Z57QzxpeR+nBsqTP3UEaBU6bS+5Kb1VSsyShNwrrZHYqLizz/Tt1kL/6cdjHPTfS
-        tQWVYrmm3ok9Nns4d0iXrKYgjy6myQzCsplFAMfOEVEiIuCl6rYVSAlk6l5PdPcF
-        PseKUgzbFbS9bZvlxrFUaKnjaZC2mqUPuLk/IH2uSrW4nOQdtqvmlKXBx4Ot2/Un
-        hw4EbNX/3aBd7YdStysVAq45pmp06drE57xNNB6pXE0zX5IJL4hmXXeXxx12E6nV
-        5fEWCRE11azbJHFwLJhWC9kXtNHjUStedejV0NxPNO3CBWaAocvmMw==
-        -----END CERTIFICATE-----
-        EOM
-
-        install_cert_on_windows(sut, 'geotrustglobal', geotrust_global_ca)
-      end
+      # There are currently no known Windows errata to apply
+      STDERR.puts '== SKIPPING Windows errata (none known)' if ENV['BEAKER_helpers_verbose']
     end
 
     # Configure and reboot SUTs into FIPS mode
