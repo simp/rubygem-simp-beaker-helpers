4dccbd44ba694a6b77b6b159c53da27144a26430
Steven Pritchard <steve@sicura.us>
2025-11-15 17:49:03 -0600

    Cleanup for rubocop

--- spec/acceptance/suites/puppet_collections/00_default_spec.rb
+++ spec/acceptance/suites/puppet_collections/00_default_spec.rb
@@ -1,19 +1,21 @@
 require 'spec_helper_acceptance'
 
-unless ENV['PUPPET_VERSION'] || ENV['BEAKER_PUPPET_COLLECTION']
-  raise('You must set either PUPPET_VERSION or BEAKER_PUPPET_COLLECTION as an environment variable')
-end
-
-if ENV['BEAKER_PUPPET_COLLECTION']
-  target_version = ENV['BEAKER_PUPPET_COLLECTION'][%r{(\d+)$}, 1]
-elsif ENV['PUPPET_VERSION']
-  target_version = ENV['PUPPET_VERSION'].split('.').first
-end
-
 hosts.each do |host|
   describe 'make sure puppet version is valid' do
     context "on #{host}" do
-      client_puppet_version = on(host, 'puppet --version').output.lines.last.strip
+      let(:target_version) do
+        unless ENV['PUPPET_VERSION'] || ENV['BEAKER_PUPPET_COLLECTION']
+          raise('You must set either PUPPET_VERSION or BEAKER_PUPPET_COLLECTION as an environment variable')
+        end
+
+        if ENV['BEAKER_PUPPET_COLLECTION']
+          ENV['BEAKER_PUPPET_COLLECTION'][%r{(\d+)$}, 1]
+        elsif ENV['PUPPET_VERSION']
+          ENV['PUPPET_VERSION'].split('.').first
+        end
+      end
+
+      let(:client_puppet_version) { on(host, 'puppet --version').output.lines.last.strip }
 
       it "is running puppet version #{target_version}" do
         expect(Gem::Version.new(client_puppet_version)).to be >= Gem::Version.new(target_version)
