7706b78fdbaa015815261444c685805b974d5e3a
Steven Pritchard <steve@sicura.us>
2025-11-17 09:22:06 -0600

    Refactor

--- spec/acceptance/suites/puppet_collections/00_default_spec.rb
+++ spec/acceptance/suites/puppet_collections/00_default_spec.rb
@@ -1,20 +1,20 @@
 require 'spec_helper_acceptance'
 
+def target_version
+  unless ENV['PUPPET_VERSION'] || ENV['BEAKER_PUPPET_COLLECTION']
+    raise('You must set either PUPPET_VERSION or BEAKER_PUPPET_COLLECTION as an environment variable')
+  end
+
+  if ENV['BEAKER_PUPPET_COLLECTION']
+    ENV['BEAKER_PUPPET_COLLECTION'][%r{(\d+)$}, 1]
+  elsif ENV['PUPPET_VERSION']
+    ENV['PUPPET_VERSION'].split('.').first
+  end
+end
+
 hosts.each do |host|
   describe 'make sure puppet version is valid' do
     context "on #{host}" do
-      let(:target_version) do
-        unless ENV['PUPPET_VERSION'] || ENV['BEAKER_PUPPET_COLLECTION']
-          raise('You must set either PUPPET_VERSION or BEAKER_PUPPET_COLLECTION as an environment variable')
-        end
-
-        if ENV['BEAKER_PUPPET_COLLECTION']
-          ENV['BEAKER_PUPPET_COLLECTION'][%r{(\d+)$}, 1]
-        elsif ENV['PUPPET_VERSION']
-          ENV['PUPPET_VERSION'].split('.').first
-        end
-      end
-
       let(:client_puppet_version) { on(host, 'puppet --version').output.lines.last.strip }
 
       it "is running puppet version #{target_version}" do
