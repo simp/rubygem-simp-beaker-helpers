34efcd6db1377c121577c6f4c59055e751da28a3
Mike Riddle <mike@sicura.us>
2026-06-25 10:40:53 -0500

    (#268) Removed unsupported SSG files (#269)
    Fixes #268

--- lib/simp/beaker_helpers.rb
+++ lib/simp/beaker_helpers.rb
@@ -14,7 +14,6 @@ module Simp::BeakerHelpers # rubocop:disable Style/OneClassPerFile
   require 'simp/beaker_helpers/constants'
   require 'simp/beaker_helpers/inspec'
   require 'simp/beaker_helpers/snapshot'
-  require 'simp/beaker_helpers/ssg'
   require 'simp/beaker_helpers/version'
   require 'find'
 
